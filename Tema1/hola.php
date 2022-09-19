94e9518e133b09c04f34d5b46d39559f4c00c811
AdrianTeru2001 <adrianteru2001@gmail.com>
2022-09-19 13:55:53 +0200

    Nuevo Archivo PHP

---
+++ Tema1/hola.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+    <!-- Muestra una frase con HTML -->
+    Hola mundo<br>
+    <p>Esto es un parrafo</p>
+    <p>Esto es un segundo parrafo</p>
+    <!-- Muestra una frase con PHP -->
+    <?php
+    echo "Me llamo Adrián Teruel";
+    ?>
+</body>
+</html> 

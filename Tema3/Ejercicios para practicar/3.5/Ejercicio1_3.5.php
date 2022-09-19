e5e718e78bdcac8f376f9591c728a874411dec56
AdrianTeru2001 <adrianteru2001@gmail.com>
2022-09-19 16:57:56 +0200

    Creación del ejercicio 1 del punto 3.5

---
+++ Tema3/Ejercicios para practicar/3.5/Ejercicio1_3.5.php
@@ -0,0 +1,16 @@
+<!-- Ejercicio 1
+Realiza un programa que pida dos números y luego muestre el resultado de su multiplicación. -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1 _ 3.5</title>
+</head>
+
+<body>
+    
+</body>
+
+</html>

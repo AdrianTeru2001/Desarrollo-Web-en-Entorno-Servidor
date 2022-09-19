04e9e2739539b5ee34adfe5ad713e7ed5106f775
AdrianTeru2001 <adrianteru2001@gmail.com>
2022-09-19 14:13:07 +0200

    Ejercicio 1 para practicar

---
+++ Tema3/Ejercicios para practicar/Ejercicio1.php
@@ -0,0 +1,18 @@
+<!-- Ejercicio 1
+Escribe un programa que muestre tu nombre por pantalla. Utiliza código PHP. -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+
+<body>
+    <?php
+        echo "Me llamo Adrián Teruel"
+    ?>
+</body>
+
+</html>

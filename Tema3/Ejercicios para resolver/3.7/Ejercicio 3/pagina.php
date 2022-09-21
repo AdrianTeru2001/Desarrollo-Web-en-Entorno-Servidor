fca2e219616c8797b967948089374ba05af680eb
AdrianTeru2001 <adrianteru2001@gmail.com>
2022-09-21 11:02:49 +0200

    Ejercicio 3 del punto 3.7 terminado

---
+++ Tema3/Ejercicios para resolver/3.7/Ejercicio 3/pagina.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nueva Pagina Editada</title>
+</head>
+
+<body>
+    <?php 
+        $color = $_GET["color"];
+        $tipo = $_GET["tipo"];
+        $alineado = $_GET["alineado"];
+        echo "<img src='$imagen' alt='imagen' width='600px' height='400px' >";
+        echo "<style> *{ background-color: $color; } p{ font-family: $tipo; text-align: $alineado; }</style>";
+    ?>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt autem, suscipit
+        eos ratione tempora magni ut tenetur provident voluptatibus quibusdam, a, facilis
+        nisi quisquam. Amet adipisci, veritatis similique suscipit pariatur culpa in
+        voluptas autem maiores molestias quis, repellendus repudiandae esse quas perspiciatis
+        beatae veniam nisi nulla voluptatibus voluptates ex doloribus?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt autem, suscipit
+        eos ratione tempora magni ut tenetur provident voluptatibus quibusdam, a, facilis
+        nisi quisquam. Amet adipisci, veritatis similique suscipit pariatur culpa in
+        voluptas autem maiores molestias quis, repellendus repudiandae esse quas perspiciatis
+        beatae veniam nisi nulla voluptatibus voluptates ex doloribus?
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt autem, suscipit
+        eos ratione tempora magni ut tenetur provident voluptatibus quibusdam, a, facilis
+        nisi quisquam. Amet adipisci, veritatis similique suscipit pariatur culpa in
+        voluptas autem maiores molestias quis, repellendus repudiandae esse quas perspiciatis
+        beatae veniam nisi nulla voluptatibus voluptates ex doloribus?
+    </p>
+</body>
+
+</html>

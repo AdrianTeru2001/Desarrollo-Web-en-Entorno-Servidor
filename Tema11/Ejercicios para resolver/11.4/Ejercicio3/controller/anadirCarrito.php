4444d26c6d0b0546a12c9863ba3c023360645d22
AdrianTeru2001 <adrianteru2001@gmail.com>
2022-12-15 09:04:11 +0100

    Modificacion

--- Tema11/Ejercicios para resolver/11.4/Ejercicio3/controller/anadirCarrito.php
+++ Tema11/Ejercicios para resolver/11.4/Ejercicio3/controller/anadirCarrito.php
@@ -1,7 +1,7 @@
 <?php
     
     require_once "../model/Producto.php";
-    //En este caso al crear el objeto, el stock precio es la cantidad que vamos a meter en el carrito
+    //En este caso al crear el objeto, el stock precio es la cantidad que vamos a meter en el carrito en la base de datos
     $productoAux = new Producto($_POST["id"], $_POST["nom"], $_POST["cant"], $_POST["stock"]);
     $productoAux->anadirCarrito();
 
